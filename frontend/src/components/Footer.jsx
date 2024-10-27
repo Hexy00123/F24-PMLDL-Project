eab2d459883c6a772f15a190e9232e0aee8c3cce
Ruslan Izmailov <r.izmajlov2018@yandex.ru>
2024-10-27 14:21:41 +0300

    feat: frontend

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p className="footer-text">
+                Enjoy your drink ☕🥐
+            </p>
+        </div>
+    )
+}
+
+export default Footer
