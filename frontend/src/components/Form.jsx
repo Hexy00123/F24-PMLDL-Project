157daddedc508fbcd0cf5d9083fb67734e1adf58
Ruslan Izmailov <r.izmajlov2018@yandex.ru>
2024-11-18 19:02:46 +0300

    feat: покрасил кнопки

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -7,7 +7,7 @@ const Form = ({ generateImage }) => {
     return (
         <div className="form-section">
             <div className="container-title">
-                <h1 className="title">Какой та загаловак</h1>
+                <h1 className="title">Enter drink recipie</h1>
             </div>
             <div className="form-container">
                 <div className="input-div">
@@ -16,7 +16,7 @@ const Form = ({ generateImage }) => {
                         <textarea name="prompt" id="" cols="30" rows="4" value={prompt} onChange={(e) => setPrompt(e.target.value)} className="form-control" placeholder="Café con Leche: Equal parts espresso and steamed milk."></textarea>
                     </div>
                 </div>
-                <button className='btn' onClick={() => generateImage(prompt, setPrompt)}>Generate ✨</button>
+                <button className='btn' onClick={() => generateImage(prompt, setPrompt)}>Generate</button>
             </div>
         </div >
     )
