cfba53e8497d27708e3f4181644f7b6c2cf5b21a
Ruslan Izmailov <r.izmajlov2018@yandex.ru>
2024-11-24 18:06:31 +0300

    fix: download button raises error when no image

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -3,6 +3,8 @@ import { v4 as uuidv4 } from 'uuid';
 
 
 export async function downloadImage(photo) {
-	const _id = uuidv4();
-	FileSaver.saveAs(photo, `download-${_id}.jpg`);
+	try {
+		const _id = uuidv4();
+		FileSaver.saveAs(photo, `download-${_id}.jpg`);
+	} catch (error) {}
 }
